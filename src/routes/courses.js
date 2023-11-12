4c4ccd387be428706092ec634c90769fa1d3eeb5
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-11-12 16:02:21 -0600

    coourses routes added and successful with bearerAuth and acl middleware

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -22,4 +22,46 @@ router.post('/course', bearerAuth, acl('create'), async (req, res, next) => {
   
 }); 
 
+// Route to get all courses
+router.get('/course', bearerAuth, acl('read'), async (req, res, next) => {
+    try {
+        const courses = await coursesCollection.read();
+        res.status(200).send(courses);
+    } catch (e) {
+        console.log(e);
+        next();
+    }
+});
+
+// Route to update a specific course by ID
+router.put('/course/:id', bearerAuth, acl('update'), async (req, res, next) => {
+    try {
+        const updatedCourse = await coursesCollection.update(req.params.id, req.body);
+        if (updatedCourse) {
+            res.status(200).send(updatedCourse);
+
+        } else {
+            res.status(404).send({ message: "Course not found"})
+        }
+    } catch (e) {
+        next(e);
+    }
+});
+
+// Route to delete a specific course by ID
+router.delete('/course/:id', bearerAuth, acl('delete'), async (req, res, next) => {
+    try {
+        const result = await coursesCollection.delete(req.params.id);
+        if(result.message === 'Record deleted successfully') {
+            res.status(200).send(result);
+        } else {
+            res.status(404).send({ message: "Course not found or not deleted"});
+        }
+    } catch (e) {
+        next(e);
+    }
+});
+
+
+
 module.exports = router;
